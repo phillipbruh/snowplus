d096589e40d60802ad051a5671a3ea7096d6d2fc
Discord <95700058+phillipbruh@users.noreply.github.com>
2022-03-14 15:31:01 -0400

    Update demon.user.js

--- demon.user.js
+++ demon.user.js
@@ -31,13 +31,19 @@
 // ==/UserScript==
 
 
-
 var localkey = GM_getValue('key')
 
 if (localkey === "" || localkey === null || localkey === undefined) {
     localkey = prompt("Please enter your FIRST name with NO CAPITALS or SPACES or this will not work once you put your name and do alt+i if the menu does not work it means you do not have a account. create one here: https://forms.gle/gd4eUD62KWCmG8BEA");
-    window.open("https://forms.gle/gd4eUD62KWCmG8BEA");
     GM_setValue('key', localkey)
+    alert("welcome to demon menu here is a tutorial!");
+    alert("open a new tab and goto any website");
+    alert("once you are there do alt+i to open the menu");
+    alert("to use a hack click the button with the hack you want to use");
+    alert("finaly to close the menu do alt+x");
+    alert("thats it!");
+    window.open("https://forms.gle/gd4eUD62KWCmG8BEA");
+    
     var message = 5
     GM_setValue('message', message)
 }
