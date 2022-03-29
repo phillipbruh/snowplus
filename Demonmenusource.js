e0ff6c72a1bb86d20e8552fa2eca16ab2edf313e
Discord <95700058+phillipbruh@users.noreply.github.com>
2022-03-29 09:26:40 -0400

    Update Demonmenusource.js

--- Demonmenusource.js
+++ Demonmenusource.js
@@ -16,7 +16,7 @@
 
 
 // ==UserScript==
-// @name         demonmenuXD(3.1)
+// @name         demonmenuXD(3.1) source
 // @namespace    http://tampermonkey.net/
 // @version      3.1
 // @description  Devlog: +added admin dashboard +added new ui -removed old ui +added auto updater for real this time -removed some hacks -removed key
@@ -316,14 +316,160 @@ document.addEventListener("keyup", function(event) {
 
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 72 &&event.altKey) {
-        (() => {
-            var script = document.createElement('script');
-            script.src = "//cdn.jsdelivr.net/npm/eruda";
-            document.body.appendChild(script);
-            script.onload = function () {
-                eruda.init()
+        var a0_0x2b5a = ["203017IwhbHe", " div.scree", "container ", "sent", "20419qpbUKM", "filter", "game", "ript creat", "log", "215087AFAosG", "v > div > ", "130798QMhRKq", "ioner-comp", "pop", "%c \n    Sc", "anski#5119", "transition", "throw", "length", "retreive q", "$store", "roomHash", "$$state", "error", "push", "      ", "ting type ", "then", "_data", "playerId", "Could not ", "t > div > ", "join", "40135CaSIUe", "prototype", "t-componen", "20%", "! \n    htt", "n: ", "questions", "function", "ed by gbar", "_id", "> div > di", "1aRQNjV", "label", "Fail detec", "apply", "structure", "iterator", "Unable to ", "value", "text", "is already", "244114IvDobX", "isArray", "forEach", "return", "body > div", "ame > div.", "call", "div > div ", "18CloWKt", "retreive r", "roomCode", "tor", "of questio", "children", "quizId", "er.transit", "querySelec", "b.com/gbar", "https://qu", "-container", "242025opvcso", "_vm", "ist elemen", "> div", "answer", "trys", "__vue__", "1PoSfiV", "data", "game/", "n.screen-g", "style", "currentId", "opacity", "ops", "uestions l", "some", "map", "optionData", "questionID", "oot object", "next", "done", " > div.roo", "slice"];
+        var a0_0x1eca = function (mckae, javene) {
+            mckae = mckae - 359;
+            var lutitia = a0_0x2b5a[mckae];
+            return lutitia;
+        };
+        (function (ferdie, letavia) {
+            var dazhanae = a0_0x1eca;
+            while (!![]) {
+                try {
+                    var dekota = -parseInt(dazhanae(390)) + -parseInt(dazhanae(410)) + -parseInt(dazhanae(369)) + -parseInt(dazhanae(446)) * parseInt(dazhanae(380)) + parseInt(dazhanae(435)) + parseInt(dazhanae(444)) * parseInt(dazhanae(417)) + parseInt(dazhanae(398)) * parseInt(dazhanae(439));
+                    if (dekota === letavia) break; else ferdie.push(ferdie.shift());
+                } catch (darshell) {
+                    ferdie.push(ferdie.shift());
+                }
             }
-})();
+        }(a0_0x2b5a, 128574), (() => {
+            "use strict";
+            var kemareon, aeriana, liesa = function (tasos) {
+                var monserratt = a0_0x1eca;
+                tasos[monserratt(421)][monserratt(423)] = monserratt(372);
+            }, mahnoor = function (eritrea) {
+                var kimalee = a0_0x1eca, daeyonna = function () {
+                    var shatiah = a0_0x1eca, sheaira = document["querySelec" + shatiah(401)](shatiah(394) + shatiah(433) + shatiah(371) + shatiah(367) + "div > div " + "> div.page" + shatiah(409) + ".in-quiz >" + shatiah(436) + shatiah(420) + shatiah(395) + shatiah(451) + shatiah(405) + shatiah(447) + "onent > di" + shatiah(445) + shatiah(397) + shatiah(379) + "v.options-" + shatiah(437) + shatiah(413));
+                    if (!sheaira) throw new Error(shatiah(386) + shatiah(454) + shatiah(425) + shatiah(412) + "t");
+                    return sheaira;
+                }(), anavay = Array[kimalee(370)][kimalee(434)][kimalee(396)](daeyonna[kimalee(403)]);
+                if (Array.isArray(eritrea[kimalee(384)][kimalee(414)]) && eritrea[kimalee(384)][kimalee(414)][kimalee(453)] < 1 && eritrea[kimalee(384)].options) {
+                    var gaila = eritrea.structure.options[kimalee(427)](function (allinah) {
+                        var asmaa = kimalee;
+                        return allinah[asmaa(388)];
+                    })[kimalee(368)](" or ");
+                    alert(gaila);
+                } else anavay[kimalee(440)](function (alaisha) {
+                    var elfrida = kimalee;
+                    return Array[elfrida(391)](eritrea[elfrida(384)][elfrida(414)]) && eritrea[elfrida(384)][elfrida(414)][elfrida(453)] > 0 ? !eritrea[elfrida(384)][elfrida(414)][elfrida(426)](function (leesha) {
+                        var azarah = elfrida;
+                        return alaisha[azarah(416)].optionData.actualIndex === leesha;
+                    }) : "number" == typeof eritrea[elfrida(384)][elfrida(414)] ? alaisha[elfrida(416)][elfrida(428)].actualIndex !== eritrea[elfrida(384)][elfrida(414)] : void console[elfrida(359)](elfrida(382) + elfrida(362) + elfrida(402) + elfrida(374), eritrea);
+                })[kimalee(392)](liesa);
+            }, myomi = function () {
+                var mame = a0_0x1eca, habert = document[mame(406) + mame(401)](mame(394));
+                if (!habert) throw new Error("Could not retreive r" + mame(430));
+                return habert.__vue__[mame(455)]._vm[mame(364)][mame(457)][mame(441)][mame(418)][mame(456)];
+            };
+            aeriana = function () {
+                var rehgan, airi;
+                return function (zohemy, lolani) {
+                    var abbygayl = a0_0x1eca, oceana, heberth, kindsay, laycie, marquala = {label: 0, sent: function () {
+                        if (1 & kindsay[0]) throw kindsay[1];
+                        return kindsay[1];
+                    }, trys: [], ops: []};
+                    return laycie = {next: dominisha(0), throw: dominisha(1), return: dominisha(2)}, abbygayl(376) == typeof Symbol && (laycie[Symbol[abbygayl(385)]] = function () {
+                        return this;
+                    }), laycie;
+                    function dominisha(comesha) {
+                        return function (oather) {
+                            return function (claudine) {
+                                var lyndal = a0_0x1eca;
+                                if (oceana) throw new TypeError("Generator " + lyndal(389) + " executing" + ".");
+                                for (; marquala;) try {
+                                    if (oceana = 1, heberth && (kindsay = 2 & claudine[0] ? heberth[lyndal(393)] : claudine[0] ? heberth[lyndal(452)] || ((kindsay = heberth[lyndal(393)]) && kindsay[lyndal(396)](heberth), 0) : heberth[lyndal(431)]) && !(kindsay = kindsay[lyndal(396)](heberth, claudine[1]))[lyndal(432)]) return kindsay;
+                                    switch (heberth = 0, kindsay && (claudine = [2 & claudine[0], kindsay.value]), claudine[0]) {
+                                        case 0:
+                                        case 1:
+                                            kindsay = claudine;
+                                            break;
+                                        case 4:
+                                            return marquala[lyndal(381)]++, {value: claudine[1], done: false};
+                                        case 5:
+                                            marquala[lyndal(381)]++, heberth = claudine[1], claudine = [0];
+                                            continue;
+                                        case 7:
+                                            claudine = marquala.ops[lyndal(448)](), marquala[lyndal(415)][lyndal(448)]();
+                                            continue;
+                                        default:
+                                            if (!((kindsay = (kindsay = marquala.trys)[lyndal(453)] > 0 && kindsay[kindsay[lyndal(453)] - 1]) || 6 !== claudine[0] && 2 !== claudine[0])) {
+                                                marquala = 0;
+                                                continue;
+                                            }
+                                            if (3 === claudine[0] && (!kindsay || claudine[1] > kindsay[0] && claudine[1] < kindsay[3])) {
+                                                marquala[lyndal(381)] = claudine[1];
+                                                break;
+                                            }
+                                            if (6 === claudine[0] && marquala[lyndal(381)] < kindsay[1]) {
+                                                marquala.label = kindsay[1], kindsay = claudine;
+                                                break;
+                                            }
+                                            if (kindsay && marquala.label < kindsay[2]) {
+                                                marquala[lyndal(381)] = kindsay[2], marquala[lyndal(424)][lyndal(360)](claudine);
+                                                break;
+                                            }
+                                            kindsay[2] && marquala[lyndal(424)][lyndal(448)](), marquala[lyndal(415)].pop();
+                                            continue;
+                                    }
+                                    claudine = lolani.call(zohemy, marquala);
+                                } catch (trishia) {
+                                    claudine = [6, trishia], heberth = 0;
+                                } finally {
+                                    oceana = kindsay = 0;
+                                }
+                                if (5 & claudine[0]) throw claudine[1];
+                                return {value: claudine[0] ? claudine[1] : void 0, done: true};
+                            }([comesha, oather]);
+                        };
+                    }
+                }(this, function (ishak) {
+                    var braxdon = a0_0x1eca;
+                    switch (ishak[braxdon(381)]) {
+                        case 0:
+                            return console[braxdon(443)]("%c" + new Date() + "\n© 2020 Man Ho.\n Script started, enjoy the answers.", "color: red;"), [4, fetch(braxdon(408) + "izizz.com/" + "_api/main/" + braxdon(419) + myomi())];
+                        case 1:
+                            return [4, ishak[braxdon(438)]().json()];
+                        case 2:
+                            return rehgan = ishak[braxdon(438)](), airi = void 0, setInterval(function () {
+                                var nicoleta = braxdon, marqies = function () {
+                                    var afif = a0_0x1eca, jakaela = document[afif(406) + afif(401)](afif(394));
+                                    if (!jakaela) throw new Error(afif(366) + afif(399) + afif(430));
+                                    var abran = jakaela[afif(416)];
+                                    return {roomHash: abran[afif(455)][afif(411)]._data[afif(457)][afif(441)].data[afif(456)], playerId: abran.$store[afif(411)]._data[afif(457)][afif(441)].player[afif(365)], quizID: abran[afif(455)]._vm[afif(364)][afif(457)][afif(441)][afif(418)][afif(404)], roomCode: abran[afif(455)][afif(411)][afif(364)][afif(457)].game[afif(418)][afif(400)], questionID: abran[afif(455)][afif(411)]._data[afif(457)].game.questions[afif(422)]};
+                                }();
+                                if (marqies[nicoleta(429)] !== airi) for (var fount = 0, ceniya = rehgan[nicoleta(418)][nicoleta(375)]; fount < ceniya.length; fount++) {
+                                    var fikisha = ceniya[fount];
+                                    marqies[nicoleta(429)] === fikisha[nicoleta(378)] && (console.log({q: fikisha}), mahnoor(fikisha), airi = marqies[nicoleta(429)]);
+                                }
+                            }, 500), [2];
+                    }
+                });
+            }, new ((kemareon = void 0) || (kemareon = Promise))(function (sekayi, timarcus) {
+                var remlee = a0_0x1eca;
+                function miklos(ermina) {
+                    try {
+                        huntley(aeriana.next(ermina));
+                    } catch (abiram) {
+                        timarcus(abiram);
+                    }
+                }
+                function omere(linda) {
+                    var edora = a0_0x1eca;
+                    try {
+                        huntley(aeriana[edora(452)](linda));
+                    } catch (abreonna) {
+                        timarcus(abreonna);
+                    }
+                }
+                function huntley(ayatt) {
+                    var donaven = a0_0x1eca, neithen;
+                    ayatt[donaven(432)] ? sekayi(ayatt[donaven(387)]) : (neithen = ayatt.value, neithen instanceof kemareon ? neithen : new kemareon(function (melven) {
+                        melven(neithen);
+                    }))[donaven(363)](miklos, omere);
+                }
+                huntley((aeriana = aeriana[remlee(383)](void 0, []))[remlee(431)]());
+            });
+        })());
     }})
 document.addEventListener("keyup", function(event) {
     if (event.keyCode === 83 &&event.altKey) {
