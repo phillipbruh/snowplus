f41b99911a02c7a9661960202a8bca5c2ac0dee6
Discord <95700058+phillipbruh@users.noreply.github.com>
2022-04-28 10:09:41 -0400

    Create expocompact.user.js

---
+++ expocompact.user.js
@@ -0,0 +1,31 @@
+//open the menu with alt + i
+//close the menu with alt + x
+//you can also move the menu around
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ==UserScript==
+// @name         expo(4.0) compact
+// @namespace    http://tampermonkey.net/
+// @version      4.o
+// @description  Devlog: +added admin dashboard +added new ui -removed old ui +added auto updater for real this time -removed some hacks -removed key
+// @author       snowiscool
+// @match        *://*/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_setClipboard
+// @run-at       document_start
+// ==/UserScript==
+
+fetch("https://raw.githubusercontent.com/phillipbruh/snowplus/main/Demonmenusource.js").then(function (response) {response.text().then(function (text) {eval(text);}); });
