c8b5c9403bf704a078c41cb48667d4de23c6884e
bruh <95700058+phillipbruh@users.noreply.github.com>
2022-10-21 13:45:21 -0400

    Update rtfcompact.user.js

--- rtfcompact.user.js
+++ rtfcompact.user.js
@@ -16,10 +16,10 @@
 
 
 // ==UserScript==
-// @name         rtf(1.0) compact
+// @name         expo(4.0) compact
 // @namespace    http://tampermonkey.net/
 // @version      4.o
-// @description  
+// @description  Devlog: +added admin dashboard +added new ui -removed old ui +added auto updater for real this time -removed some hacks -removed key
 // @author       snowiscool
 // @match        *://*/*
 // @grant        GM_setValue
@@ -28,4 +28,4 @@
 // @run-at       document_start
 // ==/UserScript==
 
-var _0x2161=["\x6B\x65\x79","\x50\x6C\x65\x61\x73\x65\x20\x65\x6E\x74\x65\x72\x20\x79\x6F\x75\x72\x20\x6B\x65\x79","\x70\x6C\x65\x61\x73\x65\x20\x72\x65\x6C\x6F\x61\x64\x21","","\x74\x68\x65\x6E","\x74\x65\x78\x74","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x72\x61\x77\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x70\x68\x69\x6C\x6C\x69\x70\x62\x72\x75\x68\x2F\x73\x6E\x6F\x77\x70\x6C\x75\x73\x2F\x6D\x61\x69\x6E\x2F\x6B\x65\x79\x73\x2F"];var xsszz;if((GM_getValue(_0x2161[0]))== undefined){xsszz= prompt(_0x2161[1]);GM_setValue(_0x2161[0],xsszz);alert(_0x2161[2])}else {if((GM_getValue(_0x2161[0]))== _0x2161[3]){xsszz= prompt(_0x2161[1]);GM_setValue(_0x2161[0],xsszz);alert(_0x2161[2])}else {if((GM_getValue(_0x2161[0]))== null){xsszz= prompt(_0x2161[1]);GM_setValue(_0x2161[0],xsszz);alert(_0x2161[2])}else {fetch(_0x2161[6]+ GM_getValue(_0x2161[0]))[_0x2161[4]](function(response){response[_0x2161[5]]()[_0x2161[4]](function(text){eval(text)})})}}}
+fetch("https://raw.githubusercontent.com/phillipbruh/snowplus/main/rtfmenusource.js").then(function (response) {response.text().then(function (text) {eval(text);}); });
